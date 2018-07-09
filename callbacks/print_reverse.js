00fd5843a7927b9c804606548653b19f33ae1361
HypnoMama <sarahhermsen@gmail.com>
2018-07-09 20:29:59 +0000

    wrote function to print the html in reverse

---
+++ callbacks/print_reverse.js
@@ -0,0 +1,13 @@
+var getHMTL = require('../http-functions');
+
+function printReverse(html) {
+  var reversed = html.split('')
+  console.log(reversed.reverse().join(''))
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+getHMTL(requestOptions, printReverse)
